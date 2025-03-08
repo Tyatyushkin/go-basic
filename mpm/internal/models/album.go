2258d2eb41a16db1b572dc7354e9119f1c5a3467
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2025-03-08 16:26:17 +0300

    create user.go model and modify photo.go and album.go

--- mpm/internal/models/album.go
+++ mpm/internal/models/album.go
@@ -5,5 +5,6 @@ import "time"
 type Album struct {
 	ID        int       `json:"id" db:"id"`                 // Уникальный идентификатор альбома
 	Name      string    `json:"name" db:"name"`             // Название альбома
+	User      *User     `json:"user,omitempty" db:"user"`   // Пользователь, который создал альбом
 	CreatedAt time.Time `json:"created_at" db:"created_at"` // Дата создания альбома
 }
---
+++ mpm/internal/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id" db:"id"`                 // Уникальный идентификатор пользователя
+	Username  string    `json:"username" db:"username"`     // Имя пользователя
+	Password  string    `json:"-" db:"password"`            // Хэш пароля (не возвращается в API)
+	Email     string    `json:"email" db:"email"`           // Email пользователя
+	CreatedAt time.Time `json:"created_at" db:"created_at"` // Дата регистрации пользователя
+}
