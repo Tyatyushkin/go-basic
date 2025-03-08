3813999b4088ef715a2f042c24b5b17224d7c609
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2025-03-08 16:21:09 +0300

    add Album and StorageType to model photo.go

--- mpm/internal/models/photo.go
+++ mpm/internal/models/photo.go
@@ -3,8 +3,10 @@ package models
 import "time"
 
 type Photo struct {
-	ID        int       `json:"id" db:"id"`     // Уникальный идентификатор фотографии
-	Name      string    `json:"name" db:"name"` // Название фотографии
-	Path      string    `json:"path" db:"path"` // Путь к фотографии (локальный или url)
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	ID          int       `json:"id" db:"id"`                     // Уникальный идентификатор фотографии
+	Name        string    `json:"name" db:"name"`                 // Название фотографии
+	Path        string    `json:"path" db:"path"`                 // Путь к фотографии (локальный или url)
+	Album       *Album    `json:"album,omitempty" db:"album"`     // Альбом, к которому принадлежит фотография
+	StorageType string    `json:"storage_type" db:"storage_type"` // Тип хранения фотографии (local, google, dropbox)
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
 }
