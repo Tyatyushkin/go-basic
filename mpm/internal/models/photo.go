ebe6e5b017737b613902ffc545e735ab29221147
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2025-03-14 18:46:19 +0300

    modify models and create interface provider

--- mpm/internal/models/photo.go
+++ mpm/internal/models/photo.go
@@ -3,11 +3,13 @@ package models
 import "time"
 
 type Photo struct {
-	ID          int       `json:"id" db:"id"`                     // Уникальный идентификатор фотографии
-	Name        string    `json:"name" db:"name"`                 // Название фотографии
-	Path        string    `json:"path" db:"path"`                 // Путь к фотографии (локальный или url)
-	Album       *Album    `json:"album,omitempty" db:"album"`     // Альбом, к которому принадлежит фотография
-	User        *User     `json:"user,omitempty" db:"user"`       // Пользователь, который загрузил фотографию
-	StorageType string    `json:"storage_type" db:"storage_type"` // Тип хранения фотографии (local, google, dropbox)
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	ID          int        `json:"id" db:"id"`                 // Уникальный идентификатор фотографии
+	Name        string     `json:"name" db:"name"`             // Название фотографии
+	Path        string     `json:"path" db:"path"`             // Путь к фотографии (локальный или url)
+	Album       *Album     `json:"album,omitempty" db:"album"` // Альбом, к которому принадлежит фотография
+	User        *User      `json:"user,omitempty" db:"user"`   // Пользователь, который загрузил фотографию
+	Tags        []string   `json:"tags" db:"tags"`             // Теги фотографии
+	Metadata    []Metadata `json:"metadata" db:"metadata"`
+	StorageType string     `json:"storage_type" db:"storage_type"` // Тип хранения фотографии (local, google, dropbox)
+	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
 }
